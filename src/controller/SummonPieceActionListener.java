812cea84d66b3e42e2c67e6e977887bae675eceb
Edward Kahiro Kuo <kahirokuo@gmail.com>
2019-04-08 22:47:42 +1000

    refactored placepiece

--- src/controller/SummonPieceActionListener.java
+++ src/controller/SummonPieceActionListener.java
@@ -17,102 +17,32 @@ import view.GameFrameView;
 
 // Clicking on a piece to summon
 public class SummonPieceActionListener implements ActionListener{
-	
+
 	GameFrameView frame; 
 	public boolean placing;
-	
+
 	public SummonPieceActionListener(GameFrameView frame){
 		this.frame = frame;
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		JButton source = (JButton) e.getSource();
-		
-		if(source ==  frame.spawn_General || source ==  frame.spawn_Lieutenant || source ==  frame.spawn_Footman || source ==  frame.spawn_Cannon
-				|| source ==  frame.spawn_Archer || source ==  frame.spawn_Spearman) {
-			placing = true;
-	
-			
-			switch(source.getName()) {
-			
-			case "spawn_General":
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.ONE_IMAGE)).getImage(),
-		                            new Point(0, 0), "General"));
-		        	System.out.println(source.getName());
-		            return;
-			        
-		        
-			case "spawn_Lieutenant":
-		        if (frame.getFrame().getCursor().getType() != 0) {
-		        	frame.getFrame().setCursor(DEFAULT_CURSOR);
-		            break;
-			        } 
-		        else {
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.TWO_IMAGE)).getImage(),
-		                            new Point(0, 0), "Lieutenant"));
-		            break;
-			        }
-		        
-			case "spawn_Footman":
-		        if (frame.getFrame().getCursor().getType() != 0) {
-		        	frame.getFrame().setCursor(DEFAULT_CURSOR);
-		            break;
-			        } 
-		        else {
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.THREE_IMAGE)).getImage(),
-		                            new Point(0, 0), "Footman"));
-		            break;
-			        }
-		        
-			case "spawn_Cannon":
-		        if (frame.getFrame().getCursor().getType() != 0) {
-		        	frame.getFrame().setCursor(DEFAULT_CURSOR);
-		            break;
-			        } 
-		        else {
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.FOUR_IMAGE)).getImage(),
-		                            new Point(0, 0), "Cannon"));
-		            break;
-			        }
-		        
-			case "spawn_Archer":
-		        if (frame.getFrame().getCursor().getType() != 0) {
-		        	frame.getFrame().setCursor(DEFAULT_CURSOR);
-		            break;
-			        } 
-		        else {
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.FIVE_IMAGE)).getImage(),
-		                            new Point(0, 0), "Archer"));
-		            break;
-			        }
-		        
-			case "spawn_Spearman":
-		        if (frame.getFrame().getCursor().getType() != 0) {
-		        	frame.getFrame().setCursor(DEFAULT_CURSOR);
-		            break;
-			        } 
-		        else {
-		        	frame.getFrame().setCursor(Toolkit.getDefaultToolkit().
-		                    createCustomCursor(new ImageIcon(this.getClass().getResource(frame.SIX_IMAGE)).getImage(),
-		                            new Point(0, 0), "Spearman"));
-		            break;
-			        }
-			
+		JButton[] spawns= new JButton[]{frame.spawn_General, frame.spawn_Lieutenant, frame.spawn_Footman, frame.spawn_Cannon,
+				frame.spawn_Archer, frame.spawn_Spearman
+		};
+		String[] image= new String[] {frame.ONE_IMAGE, frame.TWO_IMAGE, frame.THREE_IMAGE, frame.FOUR_IMAGE, frame.FIVE_IMAGE,
+				frame.SIX_IMAGE
+		};
+		String[] name= new String[] {"General", "Liutenant", "Footman", "Cannon", "Archer", "Spearman"
+		};
+
+		for(int i=0;i<spawns.length;i++) {
+			if(source==spawns[i]) {
+				placing = true;
+				frame.getFrame().setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new ImageIcon(
+						this.getClass().getResource(image[i])).getImage(), new Point(0, 0), name[i]));
+				System.out.println(source.getName());
 			}
-
-	       
 		}
-
 	}
-
-	       
-		
-
-
 }
