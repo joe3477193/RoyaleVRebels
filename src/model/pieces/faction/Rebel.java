751e1597f134613d03e130ece2c971eec3ac01f1
Kahiro <kahirokuo@gmail.com>
2019-04-14 22:13:04 +1000

    made rebel royale abstract

--- src/model/pieces/faction/Rebel.java
+++ src/model/pieces/faction/Rebel.java
@@ -7,7 +7,7 @@ import net.sf.oval.constraint.NotNull;
 import net.sf.oval.guard.Guarded;
 
 @Guarded
-public class Rebel extends Piece {
+public abstract class Rebel extends Piece {
 
     public Rebel(@NotNull @NotEmpty String name, @NotNull @NotEmpty String type, @NotNull @NotEmpty String code, @NotNegative int cp, @NotNegative int initHp, @NotNegative int attackPower, @NotNegative int moveSpeed, @NotNegative int attackRange,
                  boolean moveable, boolean attackable) {
