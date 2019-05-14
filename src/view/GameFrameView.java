773712a427511c8f1c22c6e33ad4bef9d25c206a
Kahiro <kahirokuo@gmail.com>
2019-05-14 23:01:45 +1000

    deleted serizable

--- src/view/GameFrameView.java
+++ src/view/GameFrameView.java
@@ -10,9 +10,7 @@ import java.awt.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class GameFrameView extends JFrame implements Serializable {
-
-    private static final long serialVersionUID = 2L;
+public class GameFrameView extends JFrame{
 
     private JFrame frame;
     private JPanel actionPanel;
