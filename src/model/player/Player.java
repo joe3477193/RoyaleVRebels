1c131bcd50a06265143ef6e4403311a38ce06abc
Huirong Huang <s3615907@student.rmit.edu.au>
2019-06-02 15:28:09 +1000

    removed CP redundant stuff from Player added "=" in the isEnoughCP function

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -2,18 +2,16 @@ package model.player;
 
 public abstract class Player {
 
-    static final int REBEL_INIT_CP = 10;
+    static final int REBEL_INIT_CP = 80;
     static final int ROYALE_INT_CP = 50;
 
     private final String faction;
     private int cp;
-    private int start_cp;
     private String name;
 
-    public Player(String name, int cp, int start_cp, String faction) {
+    public Player(String name, int cp, String faction) {
         this.name = name;
         this.cp = cp;
-        this.start_cp = start_cp;
         this.faction = faction;
     }
 
@@ -30,7 +28,7 @@ public abstract class Player {
     }
 
     public boolean isEnoughCP(int value){
-        return cp - value > 0;
+        return cp - value >= 0;
     }
 
     public String getFaction() {
